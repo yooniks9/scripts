316d80eb4630ecd09107ba717a050a15b8377bfc
Roy <yooniks9@gmail.com>
2019-02-18 15:23:31 +0800

    Create site-backup.sh

---
+++ site-backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+#get the current date
+BACKUPTIME=`date +%d-%b-%Y` 
+USER=ubuntu
+
+#create a backup file using the current date in it's name
+DESTINATION=/home/$USER/backup/html-$BACKUPTIME.tar.gz 
+
+#the folder that contains the files that we want to backup
+SOURCEFOLDER=/var/www/html 
+
+#create the backup
+tar -cpzf $DESTINATION $SOURCEFOLDER 
+
+#change ownership
+chown -R $USER:$USER $DESTINATION
+
+# Clean up files longer than 10 days [ 10 x 1440 = 14400 ] (command)
+find /home/$USER/backup/*.tar.gz -type f -mmin +14400 -exec rm {} \;
