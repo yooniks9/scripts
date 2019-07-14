79ed5ff4762cc5105bf940cc467cef9290012f3c
Roy <yooniks9@gmail.com>
2019-07-14 21:21:36 +0800

    Update sqldump.sh
    fix vars

--- sqldump.sh
+++ sqldump.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-USER="root"
-PASSWORD="CHANGEME!!!"
+DBUSER="root"
+DBPASS="CHANGEME!!!"
+DBHOST="127.0.0.1"
 #OUTPUT="/Users/yooniks/DBs"
 
 #rm "$OUTPUTDIR/*gz" > /dev/null 2>&1
 
-databases=`mysql -u $USER -p$PASSWORD -h127.0.0.1 -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
+databases=`mysql -u $DBUSER -p$DBPASS -h$DBHOST -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $USER -p$PASSWORD -h127.0.0.1 --databases $db > `date +%Y%m%d`.$db.sql
+        mysqldump -u $DBUSER -p$DBPASS -h$DBHOST --databases $db > `date +%Y%m%d`.$db.sql
        # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
