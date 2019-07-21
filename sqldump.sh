1bb7f6b451160144ce742305dd293d63007fd684
Roy <yooniks9@gmail.com>
2019-07-22 02:01:34 +0800

    add option "--skip-lock-tables"

--- sqldump.sh
+++ sqldump.sh
@@ -12,7 +12,7 @@ databases=`mysql -u $DBUSER -p$DBPASS -h$DBHOST -e "SHOW DATABASES;" | tr -d "| 
 for db in $databases; do
     if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
-        mysqldump -u $DBUSER -p$DBPASS -h$DBHOST --databases $db > `date +%Y%m%d`.$db.sql
+        mysqldump -u $DBUSER -p$DBPASS -h$DBHOST --skip-lock-tables --databases $db > `date +%Y%m%d`.$db.sql
        # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
     fi
 done
